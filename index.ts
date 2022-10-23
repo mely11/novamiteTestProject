6a79a7fc529eae268aa1b98d11093434fbbfb45a
mely11 <yiyiyuan@usc.edu>
2022-10-23 01:42:20 -0700

    Add sentinel layer to the google map

--- index.ts
+++ index.ts
@@ -3,6 +3,29 @@
 //  * Copyright 2019 Google LLC. All Rights Reserved.
 //  * SPDX-License-Identifier: Apache-2.0
 //  */
+
+import { S2L2ALayer, BBox, CRS_EPSG4326, MimeTypes, ApiType } from '@sentinel-hub/sentinelhub-js';
+
+let imageURL;
+
+const perform = async () => {
+  const layer = new S2L2ALayer({ instanceId: '85fae6a2-7590-4318-b540-a43f6b54fd95', layerId: 'L2A-LAYER' });
+  const bbox = new BBox(CRS_EPSG4326, 18, 20, 20, 22);
+
+  const getMapParams = {
+    bbox: bbox,
+    fromTime: new Date(Date.UTC(2018, 11 - 1, 22, 0, 0, 0)),
+    toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
+    width: 256,
+    height: 256,
+    format: MimeTypes.JPEG,
+  };
+  const imageBlob = await layer.getMap(getMapParams, ApiType.WMS);
+  imageURL = URL.createObjectURL(imageBlob);
+};
+perform().then(() => {});
+
+
 function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
@@ -44,15 +67,7 @@ function initMap(): void {
         return "";
       }
 
-      return [
-        "https://www.gstatic.com/io2010maps/tiles/5/L2_",
-        zoom,
-        "_",
-        coord.x,
-        "_",
-        coord.y,
-        ".png",
-      ].join("");
+      return imageURL;
     },
     tileSize: new google.maps.Size(256, 256),
   });
