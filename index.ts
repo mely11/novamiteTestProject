adf9aa9f0362d7960d7df89fda6350256937cdd8
11! <yiyiyuan@usc.edu>
2022-10-31 20:27:56 -0700

    Add google example

--- index.ts
+++ index.ts
@@ -1,78 +1,162 @@
-// /**
-//  * @license
-//  * Copyright 2019 Google LLC. All Rights Reserved.
-//  * SPDX-License-Identifier: Apache-2.0
-//  */
-
-import { S2L2ALayer, BBox, CRS_EPSG4326, MimeTypes, ApiType } from '@sentinel-hub/sentinelhub-js';
-
-let imageURL;
-
-const perform = async () => {
-  const layer = new S2L2ALayer({ instanceId: '85fae6a2-7590-4318-b540-a43f6b54fd95', layerId: 'L2A-LAYER' });
-  const bbox = new BBox(CRS_EPSG4326, 18, 20, 20, 22);
-
-  const getMapParams = {
-    bbox: bbox,
-    fromTime: new Date(Date.UTC(2018, 11 - 1, 22, 0, 0, 0)),
-    toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
-    width: 256,
-    height: 256,
-    format: MimeTypes.JPEG,
-  };
-  const imageBlob = await layer.getMap(getMapParams, ApiType.WMS);
-  imageURL = URL.createObjectURL(imageBlob);
-};
-perform().then(() => { });
-
+// This example adds hide() and show() methods to a custom overlay's prototype.
+// These methods toggle the visibility of the container <div>.
+// overlay to or from the map.
 
 function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
-      zoom: 18,
-      center: { lat: 37.783, lng: -122.403 },
+      zoom: 11,
+      center: { lat: 62.323907, lng: -150.109291 },
+      mapTypeId: "satellite",
     }
   );
 
-  const bounds: Record<number, [[number, number], [number, number]]> = {
-    17: [
-      [20969, 20970],
-      [50657, 50658],
-    ],
-    18: [
-      [41939, 41940],
-      [101315, 101317],
-    ],
-    19: [
-      [83878, 83881],
-      [202631, 202634],
-    ],
-    20: [
-      [167757, 167763],
-      [405263, 405269],
-    ],
-  };
+  const bounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(62.281819, -150.287132),
+    new google.maps.LatLng(62.400471, -150.005608)
+  );
 
-  const imageMapType = new google.maps.ImageMapType({
-    getTileUrl: function (coord, zoom) {
-      if (
-        zoom < 17 ||
-        zoom > 20 ||
-        bounds[zoom][0][0] > coord.x ||
-        coord.x > bounds[zoom][0][1] ||
-        bounds[zoom][1][0] > coord.y ||
-        coord.y > bounds[zoom][1][1]
-      ) {
-        return "";
+  // The photograph is courtesy of the U.S. Geological Survey.
+  let image = "https://developers.google.com/maps/documentation/javascript/";
+
+  image += "examples/full/images/talkeetna.png";
+
+  /**
+   * The custom USGSOverlay object contains the USGS image,
+   * the bounds of the image, and a reference to the map.
+   */
+  class USGSOverlay extends google.maps.OverlayView {
+    private bounds: google.maps.LatLngBounds;
+    private image: string;
+    private div?: HTMLElement;
+
+    constructor(bounds: google.maps.LatLngBounds, image: string) {
+      super();
+
+      this.bounds = bounds;
+      this.image = image;
+    }
+
+    /**
+     * onAdd is called when the map's panes are ready and the overlay has been
+     * added to the map.
+     */
+    onAdd() {
+      this.div = document.createElement("div");
+      this.div.style.borderStyle = "none";
+      this.div.style.borderWidth = "0px";
+      this.div.style.position = "absolute";
+
+      // Create the img element and attach it to the div.
+      const img = document.createElement("img");
+
+      img.src = this.image;
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.position = "absolute";
+      this.div.appendChild(img);
+
+      // Add the element to the "overlayLayer" pane.
+      const panes = this.getPanes()!;
+
+      panes.overlayLayer.appendChild(this.div);
+    }
+
+    draw() {
+      // We use the south-west and north-east
+      // coordinates of the overlay to peg it to the correct position and size.
+      // To do this, we need to retrieve the projection from the overlay.
+      const overlayProjection = this.getProjection();
+
+      // Retrieve the south-west and north-east coordinates of this overlay
+      // in LatLngs and convert them to pixel coordinates.
+      // We'll use these coordinates to resize the div.
+      const sw = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getSouthWest()
+      )!;
+      const ne = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getNorthEast()
+      )!;
+
+      // Resize the image's div to fit the indicated dimensions.
+      if (this.div) {
+        this.div.style.left = sw.x + "px";
+        this.div.style.top = ne.y + "px";
+        this.div.style.width = ne.x - sw.x + "px";
+        this.div.style.height = sw.y - ne.y + "px";
       }
+    }
 
-      return imageURL;
-    },
-    tileSize: new google.maps.Size(256, 256),
+    /**
+     * The onRemove() method will be called automatically from the API if
+     * we ever set the overlay's map property to 'null'.
+     */
+    onRemove() {
+      if (this.div) {
+        (this.div.parentNode as HTMLElement).removeChild(this.div);
+        delete this.div;
+      }
+    }
+
+    /**
+     *  Set the visibility to 'hidden' or 'visible'.
+     */
+    hide() {
+      if (this.div) {
+        this.div.style.visibility = "hidden";
+      }
+    }
+
+    show() {
+      if (this.div) {
+        this.div.style.visibility = "visible";
+      }
+    }
+
+    toggle() {
+      if (this.div) {
+        if (this.div.style.visibility === "hidden") {
+          this.show();
+        } else {
+          this.hide();
+        }
+      }
+    }
+
+    toggleDOM(map: google.maps.Map) {
+      if (this.getMap()) {
+        this.setMap(null);
+      } else {
+        this.setMap(map);
+      }
+    }
+  }
+
+  const overlay: USGSOverlay = new USGSOverlay(bounds, image);
+
+  overlay.setMap(map);
+
+  const toggleButton = document.createElement("button");
+
+  toggleButton.textContent = "Toggle";
+  toggleButton.classList.add("custom-map-control-button");
+
+  const toggleDOMButton = document.createElement("button");
+
+  toggleDOMButton.textContent = "Toggle DOM Attachment";
+  toggleDOMButton.classList.add("custom-map-control-button");
+
+  toggleButton.addEventListener("click", () => {
+    overlay.toggle();
   });
 
-  map.overlayMapTypes.push(imageMapType);
+  toggleDOMButton.addEventListener("click", () => {
+    overlay.toggleDOM(map);
+  });
+
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
 }
 
 declare global {
@@ -81,4 +165,3 @@ declare global {
   }
 }
 window.initMap = initMap;
-export { };
